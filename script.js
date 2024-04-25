4ee07e0655ec7c7daf94ade9e5f01e5c43e572b8
viniciusalvesnascimento <131222920+viniciusalvesnascimento@users.noreply.github.com>
2024-04-25 00:22:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+/*ATIVIDADE 1*/
+alert("Hello World")
+
+/*ATIVIDADE 2*/
+alert ("vamos fazer continhas")
+let Numberone = prompt ("Digite o primeiro numero")
+let Numbertwo = prompt ("Digite o segundo numero")
+
+let Numbers = Number(Numberone) + Number(Numbertwo)
+alert (Numbers)
+
+/*ATIVIDADE 3*/
+let weight = 60
+
+let verification1 = typeof weight === "array"  ? "Sim é um número" : "Não é um número" 
+alert( verification1)
+
+/*ATIVIDADE 4*/
+let names = "Vinicius"
+
+let verification2 = typeof names === "string" ? "Verdadeiro, é uma string" : "Falso, não é ima string"
+alert (verification2)
+
+/*ATIVIDADE 5*/
+let Beautiful = true
+
+let verification3 = typeof Beautiful === "boolean" ? "Verdadeiro, é boleano" : "Falso, não é boleano"
+alert (verification3)
+
+/*ATIVIDADE 6*/
+alert("SUBTRAÇÃO")
+let Number1 = prompt ("Digite o primeiro numero")
+let Number2 = prompt ("Digite o segundo numero")
+
+let Numberos = Number(Number1) - Number(Number2)
+alert (Numberos)
+
+/*ATIVIDADE 7*/
+alert("MULTIPLICAÇÃO")
+let Numberon_1 = prompt ("Digite o primeiro numero")
+let Numbertw_2 = prompt ("Digite o segundo numero")
+
+let Numbers_ = Number(Numberon_1) * Number(Numbertw_2)
+alert (Numbers_)
+
+/*ATIVIDADE 8*/
+alert("DIVISÃO")
+let numberone = prompt ("Digite o primeiro numero")
+let numbertwo = prompt ("Digite o segundo numero")
+
+let numbers = Number(numberone) / Number(numbertwo)
+alert (numbers)
+
+/*ATIVIDADE 9*/
+let VerifNumber1 = 16
+alert(VerifNumber1 %2 === 0 ? "número par": "numero ímpar")
+/*ATIVIDADE 10*/
+let VerifNumber2 = 55
+alert (VerifNumber2 %2 !== 0? "número ímpar":"número par")
+
